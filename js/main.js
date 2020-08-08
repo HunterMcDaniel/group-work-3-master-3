4a8a87125862856512a640ec14765e9c38c7ae85
“Hunter <hunterlmcdaniel@ufl.edu>
2020-08-08 14:40:20 -0400

    added hamburger

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// hamburger menu script
+function myFunction() {
+  var x = document.getElementById("links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+//
